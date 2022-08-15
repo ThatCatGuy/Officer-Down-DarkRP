7b791d1f98982d4855403a8070f0bb2c6c89ae60
ThatCatGuy <78601638+ThatCatGuy@users.noreply.github.com>
2022-08-15 22:57:51 +0100

    Updated and added Ping option.

--- lua/autorun/server/sv_officerdown.lua
+++ lua/autorun/server/sv_officerdown.lua
@@ -1,8 +1,15 @@
-util.AddNetworkString("PoliceDeath")
-local function BroadcastCPDeath(ply, dpos)
-	net.Start("PoliceDeath")
+CreateConVar( "officerdown_assistdelay", 120, FCVAR_SERVER_CAN_EXECUTE )
+CreateConVar( "officerdown_wanttime", 300, FCVAR_SERVER_CAN_EXECUTE )
+
+local officerdown_assistdelay = GetConVar( "officerdown_assistdelay" )
+local officerdown_wanttime = GetConVar( "officerdown_wanttime" )
+
+util.AddNetworkString("CPLocation")
+local function BroadcastCPLocation(ply, dpos, type)
+	net.Start("CPLocation")
 		net.WriteEntity(ply)
 		net.WriteVector(Vector(dpos))
+		net.WriteString(type)
 	net.Send(ply)
 end
 
@@ -27,17 +34,62 @@ function GetOfficers()
 	return cps
 end
 
-hook.Add("PlayerDeath", "PoliceDeath", function(ply, inflictor, attacker)
-	if ply != attacker and ply:isCP() then
-		if not attacker:isCP() then
-			MakeWantedPlayer(attacker, "Killing an Officer.", 300)
-		end
-		if ply:isCP() then
-		 	local cps = GetOfficers()
-		 	local deathpos = ply:GetPos()
-		    for k, v in pairs(cps) do
-		    	BroadcastCPDeath(v, deathpos)
-		    end
+local function DeathSpot(ply)
+	local cps = GetOfficers()
+	if #cps >= 1 then return end
+	if !ply:isCP() then return end
+	if !ply:Alive() then return end
+
+    if ply:isCP() then
+ 		local deathpos = ply:GetPos()
+		for k, v in pairs(cps) do
+			if v == ply then continue end
+			DarkRP.notify(v, 1, 2, "An Officer Has Been Killed!")
+			BroadcastCPLocation(v, deathpos, "cpdown")
 		end
 	end
+
+    return ""
+end
+
+local function PingPolice(ply)
+	local cps = GetOfficers()
+	if #cps == 1 then return end
+	if !ply:isCP() then return end
+	if !ply:Alive() then return end
+
+	if ply.NextPing and ply.NextPing > CurTime() then
+		local TimeLeft = ply.NextPing
+		DarkRP.notify(ply, 1, 2, "Ping Cooldown: " .. math.ceil(0 - (CurTime() - ply.NextPing)) .. " Seconds!")
+			return false
+	end
+    ply.NextPing = CurTime() + officerdown_assistdelay:GetInt()
+
+    if ply:isCP() then
+	 	local pingpos = ply:GetPos()
+		for k, v in pairs(cps) do
+	    	if v == ply then continue end
+	    	DarkRP.notify(v, 1, 4, "An Officer Has Requested Assistance!")
+	    	BroadcastCPLocation(v, pingpos, "cpping")
+	    end
+	end
+
+    return ""
+end
+
+hook.Add("PlayerDeath", "CPLocation", function(ply, inflictor, attacker)
+	if not IsValid(ply) or not IsValid(attacker) then return end
+	if ply != attacker and ply:isCP() then
+		if not attacker:isCP() then
+			MakeWantedPlayer(attacker, "Killing an Officer.", officerdown_wanttime:GetInt())
+		end
+	    DeathSpot(ply)
+	end
 end)
+
+hook.Add( "PlayerSay", "CPLocation", function( ply, text )
+    if  ( string.lower( text ) == "/backup" or string.lower( text ) == "!backup" ) then
+         PingPolice(ply)
+        return ""
+    end
+end )
