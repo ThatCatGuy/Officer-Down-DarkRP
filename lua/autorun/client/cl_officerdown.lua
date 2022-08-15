49d1a2978d6e6efd2e90e0116f451ca743b7948c
ThatCatGuy <78601638+ThatCatGuy@users.noreply.github.com>
2022-08-15 22:57:32 +0100

    Updated and added Ping option.

--- lua/autorun/client/cl_officerdown.lua
+++ lua/autorun/client/cl_officerdown.lua
@@ -5,38 +5,75 @@ local sounds = {
 	"npc/metropolice/vo/backmeupImout.wav"
 }
 
+local cvar = CreateClientConVar("officerdown_hide", 1)
+local icondelay = CreateClientConVar("officerdown_removedelay", 45)
 local deaths = {}
-local function AddHook()
-	hook.Add("HUDPaint", "PoliceDeath.Point", function()
+local pings = {}
+local delete = Material("icon16/exclamation.png")
+local ping = Material("icon16/transmit.png")
+local black = Color(10, 10, 10, 200)
+
+local function AddOfficerHook()
+	hook.Add("HUDPaint", "CPLocation", function()
+		if !LocalPlayer():Alive() then return end
+		if !cvar:GetBool() then return end
+
 		for index, posi in pairs(deaths) do
 			local point = posi:ToScreen()
-			draw.DrawText("Officer Down!","Default",point.x, point.y - 55, color_white, TEXT_ALIGN_CENTER)
+			local msg = "Officer Down!"
 			surface.SetDrawColor(255,255,255)
-			surface.SetMaterial(Material("icon16/delete.png"))
-			surface.DrawTexturedRect(point.x - 16, point.y - 32, 32, 32)
+			surface.SetMaterial(delete)
+			surface.DrawTexturedRect(point.x - 10, point.y - 20, 20, 20)
+			draw.SimpleTextOutlined(msg, "Default",point.x, point.y - 40, color_white, TEXT_ALIGN_CENTER, nil, 1, black)
+		end
+
+		for pindex, pposi in pairs(pings) do
+			local ppoint = pposi:ToScreen()
+			local msg = "Officer Needs Assistance!"
+			surface.SetDrawColor(255,255,255)
+			surface.SetMaterial(ping)
+			surface.DrawTexturedRect(ppoint.x - 10, ppoint.y - 20, 20, 20)
+			draw.SimpleTextOutlined(msg, "Default",ppoint.x, ppoint.y - 40, color_white, TEXT_ALIGN_CENTER, nil, 1, black)
 		end
 	end)
 end
 
-hook.Add("OnPlayerChangedTeam", "PoliceDeath.AddHook", function(ply, before, after)
+hook.Add("OnPlayerChangedTeam", "CPLocation.AddOfficerHook", function(ply, before, after)
 	if GAMEMODE.CivilProtection[after] then
-		AddHook()
+		AddOfficerHook()
 	elseif GAMEMODE.CivilProtection[before] then
-		hook.Remove("HUDPaint", "PoliceDeath.Point")
+		hook.Remove("HUDPaint", "CPLocation")
 	end
 end)
 
-net.Receive("PoliceDeath", function( len, pl )
+net.Receive("CPLocation", function( len, pl )
 	local ply = net.ReadEntity()
 	local pos = net.ReadVector()
+	local typ = net.ReadString()
+
+	if pl == LocalPlayer() then
+		return
+	end
 	
 	if IsValid(ply) then 
-		surface.PlaySound(sounds[math.random(#sounds)])
+		if cvar:GetBool() then
+			surface.PlaySound(sounds[math.random(#sounds)])
+		end
+		
 		local index = ply:EntIndex()
-		deaths[index] = pos
-
-		timer.Create("PoliceDeath_Point_Remove"..index, 30, 1, function()
-			deaths[index] = nil
-		end)
+		
+		if typ == "cpdown" then			
+			deaths[ply] = pos
+			timer.Create("PoliceDeath_Remove"..index, icondelay:GetInt(), 1, function()
+				deaths[ply] = nil
+			end)
+		end
+		
+		if typ == "cpping" then
+			pings[ply] = pos			
+			timer.Create("PolicePing_Remove"..index, icondelay:GetInt(), 1, function()
+				pings[ply] = nil
+			end)
+		end
 	end
 end)
